cc760d3bc936fd3727236dc4f0580dbd5f17f5f6
Bwn2 <wangaibarnabas@gmail.com>
2023-08-22 20:31:30 -0700

    Barnabas ft Ali

---
+++ main.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024
+
+void print_buffer(char buffer[], int *buff_ind);
+
+int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+
+int main(void)
+{
+    _printf("Hello, %s! This is a number: %d\n", "Alice", 42);
+    return (0);
+}
+
+int _printf(const char *format, ...)
+{
+    int i, printed = 0, printed_chars = 0;
+    int buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(list, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            printed_chars++;
+        }
+        else
+        {
+            print_buffer(buffer, &buff_ind);
+            /* Replace simulated functions with actual implementations */
+            /* int flags = get_flags(format, &i); */
+            /* int width = get_width(format, &i, list); */
+            /* int precision = get_precision(format, &i, list); */
+            /* int size = get_size(format, &i); */
+            /* ++i; */
+            /* printed = handle_print(format, &i, list, buffer, */
+            /*                      flags, width, precision, size); */
+            /* Simulated printed value */
+            printed = 5;
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
+        }
+    }
+
+    print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return (printed_chars);
+}
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+    {
+        write(1, buffer, *buff_ind);
+        *buff_ind = 0;
+    }
+}
+
